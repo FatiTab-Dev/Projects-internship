079f2c8fc47920fa2d6d68f81765574a5ce12c11
Fatima <tabtfatima8@gmail.com>
2026-06-05 21:27:14 +0100

    feat : create User and project and tasks models for database schema

---
+++ Task3-CodeAlpha/Back-end/src/models/Task.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const TaskSchema = new mongoose.Schema({
+ title: {type:String, required: true},
+ description: {type:String, required:true },
+ projectId: { type: mongoose.Schema.Types.ObjectId,ref:'Project', required: true },
+ assignedTo: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+ status:{type: String, enum:['todo', 'inProgress', 'done'], default: 'todo'},
+ dueDate: { type: Date},
+}, {timestamps:true}
+);
+
+const Task = mongoose.model('Task',TaskSchema );
+export default Task;
