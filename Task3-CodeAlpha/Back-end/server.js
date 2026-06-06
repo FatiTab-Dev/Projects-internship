110f9555aea39b4c59c63384b61990d6fe5504cb
Fatima <tabtfatima8@gmail.com>
2026-06-06 19:40:11 +0100

    done part project

--- Task3-CodeAlpha/Back-end/server.js
+++ Task3-CodeAlpha/Back-end/server.js
@@ -5,6 +5,7 @@ import helmet from 'helmet';
 import path from 'path';
 import connectDB from './src/conf/db.js';
 import authRoutes from './src/routes/authRoutes.js';
+import projectRoutes from './src/routes/projectsRoutes.js';
 const app = express();
 connectDB();
 app.use(express.json());
@@ -12,7 +13,8 @@ app.use(cors());
 app.use(helmet({
   crossOriginResourcePolicy: { policy: 'cross-origin' },
 }));
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes);
+app.use('api/projects',projectRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- Task3-CodeAlpha/Back-end/src/routes/projectsRoutes.js
+++ Task3-CodeAlpha/Back-end/src/routes/projectsRoutes.js
@@ -1,18 +1,19 @@
 import express from 'express';
 import auth from '../middleware/auth.js';
+import { createProject, getProject, getProjectById, updateProject, deleteProject, inviteMember } from '../controllers/projectController.js';
 
 const router = express.Router();
 // creat project 
-router.post('/');
+router.post('/', auth, createProject);
 //invite
-router.post('/:id/invite');
+router.post('/:id/invite',auth, inviteMember);
 // get my project
-router.get('/');
+router.get('/', auth, getProject);
 // get some project
-router.get('/:id');
+router.get('/:id', auth, getProjectById);
 // update project
-router.put('/:id');
+router.put('/:id', auth, updateProject);
 // delete project
-router.delete('/:id');
+router.delete('/:id', auth, deleteProject);
 
 export default router;
