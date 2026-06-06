67c8118461e3f54d6b1af9ca80edb525caf18762
Fatima <tabtfatima8@gmail.com>
2026-06-06 13:43:32 +0100

    feat : register login , token auth

--- Task3-CodeAlpha/Back-end/server.js
+++ Task3-CodeAlpha/Back-end/server.js
@@ -4,12 +4,15 @@ import cors from 'cors';
 import helmet from 'helmet';
 import path from 'path';
 import connectDB from './src/conf/db.js';
+import authRoutes from './src/routes/authRoutes.js';
 const app = express();
 connectDB();
+app.use(express.json());
 app.use(cors());
 app.use(helmet({
   crossOriginResourcePolicy: { policy: 'cross-origin' },
 }));
+app.use('/api/auth', authRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ Task3-CodeAlpha/Back-end/src/controllers/userController.js
@@ -0,0 +1,44 @@
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
+const generateToken = (id) => {
+    return jwt.sign({id},process.env.JWT_SECRET ,{ expiresIn: '30d' });
+}
+
+export const register = async (req, res) =>{
+    try{
+      const {name, email, password } = req.body;
+      const userExists = await User.findOne({email});
+      if (userExists) {
+       return  res.status(400).json({ message: 'User already exists' });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password,salt);
+      const user = await User.create({ name, email, password: hashedPassword});
+      res.status(201).json({
+       message: 'User registered successfully',
+       token: generateToken(user._id),
+       user: { id:user._id, name:user.name, email:user.email}
+    });
+    } catch(error){
+        res.status(500).json({message:'error during signup'});
+    };
+}
+
+export const login = async (req, res) =>{
+    try {
+        const { email, password} = req.body;
+        const user = await User.findOne ({email});
+        if (user && (await bcrypt.compare(password, user.password))){
+            res.status(200).json({
+                token: generateToken(user._id),
+                user:{ id:user._id, name: user.name, email: user.email}
+            });
+        }else{
+          res.status(401).json({message: 'Invalid email or password' });
+    }
+    } catch{
+         res.status(500).json({message:'error during login'});
+    }
+}
